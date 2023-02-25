eb0798f4e3d541243a2abf9edae8a1be01ac56e5
ChaimaeBinjach <binjachchaimaa@gmail.com>
2023-02-25 19:56:34 +0100

    I added the link of my reservation to display

--- includes/aside.php
+++ includes/aside.php
@@ -2,29 +2,25 @@
     <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
         <!-- Home Page -->
         <li class="nav-item">
-            <a href="/K-H-Parking-App/index.php" class="nav-link py-3 border-bottom rounded-0" title="Home "
-                data-bs-toggle="tooltip" data-bs-placement="right">
+            <a href="/K-H-Parking-App/index.php" class="nav-link py-3 border-bottom rounded-0" title="Home " data-bs-toggle="tooltip" data-bs-placement="right">
                 <span class="fas fa-fw fa-home bi pe-none"></span>
             </a>
         </li>
         <!-- Reservation -->
         <li class="nav-item">
-            <a href="#" class="nav-link py-3 border-bottom rounded-0" title="My Reservation" data-bs-toggle="tooltip"
-                data-bs-placement="right">
+            <a href="/K-H-Parking-App/reservation.php" class="nav-link py-3 border-bottom rounded-0" title="My Reservation" data-bs-toggle="tooltip" data-bs-placement="right">
                 <span class="fas fa-fw fa-calendar-check bi pe-none"></span>
             </a>
         </li>
         <!-- Reserve -->
         <li class="nav-item">
-            <a href="#" class="nav-link py-3 border-bottom rounded-0" title="Reserve" data-bs-toggle="tooltip"
-                data-bs-placement="right">
+            <a href="#" class="nav-link py-3 border-bottom rounded-0" title="Reserve" data-bs-toggle="tooltip" data-bs-placement="right">
                 <span class="fas fa-fw fa-bookmark bi pe-none"></span>
             </a>
         </li>
         <!-- Giveaway -->
         <li class="nav-item">
-            <a href="giveawayPage.php" class="nav-link py-3 border-bottom rounded-0" title="Giveaway"
-                data-bs-toggle="tooltip" data-bs-placement="right">
+            <a href="giveawayPage.php" class="nav-link py-3 border-bottom rounded-0" title="Giveaway" data-bs-toggle="tooltip" data-bs-placement="right">
                 <span class="fas fa-fw fa-gift bi pe-none"></span>
             </a>
         </li>
